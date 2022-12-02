5e4a2622b115488a89b25352886c7ed9d2c60bb7
shrayvaidya <85644781+shrayvaidya@users.noreply.github.com>
2022-12-02 14:17:19 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
-
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
 
 document.addEventListener("DOMContentLoaded", function(){
-    console.log("this is a website about me!");
+  console.log("this is a website about me!");
 });
 
-
-/* 
-Make the "Click me!" button move when the ++++visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    console.log("the slay button has been pressed.");
-    // The JS works in conjunction with the 'dipped' code in style.css
-    let el = document.getElementById("slaybox");
-    el.append("slay ")
-    el.style.backgroundColor = "#306E06";
-    el.style.color = "#96BE7C";
-  };
+function validateForm() {
+let x1 = document.forms["myForm"]["FirstName"].value;
+if (x2 == "") {
+  alert("Name must be filled out");
+  return false;
 }
-
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+let x2 = document.forms["myForm"]["LastName"].value;
+if (x2 == "") {
+  alert("Name must be filled out");
+  return false;
+}
+let y = document.forms["myForm"]["Email"].value;
+if (y == "") {
+  alert("Email must be filled out properly");
+  return false;
+}
+let z = document.forms["myForm"]["Message"].value;
+if (z == "") {
+  alert("Message must be filled out properly");
+  return false;
+}
+let a = document.forms["myForm"]["Location"].value;
+if (a=="") {
+  alert("Location must be filled out properly");
+  return false;
+}
+}
